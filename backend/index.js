77415e5c12c1e06597ec3a1484936b3d4b4db802
Rishabh Gautam <157235362+rishabhgautam12@users.noreply.github.com>
2025-12-18 12:14:36 +0530

    Refactor server start to use promise handling

--- backend/index.js
+++ backend/index.js
@@ -434,4 +434,7 @@ app.get("/admin/bookings", async (req, res) => {
 });
 
 //server is running
-app.listen(PORT, () => console.log("server is running at port:" + PORT));
+app.listen(PORT)
+  .then(() => console.log(`Server running on ${PORT}`))
+  .catch((err) => console.error(err));
+
